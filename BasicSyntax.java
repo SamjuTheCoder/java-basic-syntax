9e3c0e7f57bdb0e8fe92196f79cc729db9efd68d
Julius <fasema.julius@appmartgroup.com>
2024-09-26 13:37:12 +0100

    my first commit

---
+++ BasicSyntax.java
@@ -0,0 +1,26 @@
+public class BasicSyntax {
+    
+    public static void main(String[] args) {
+       
+        // declaring interger variable
+        int quantity = 10; 
+
+        //declaring double
+        double price = 234.56;
+
+        // declaring String variable
+        String product_name = "Chocolate";
+
+        // declaring char type
+        char tag = 'A';
+
+        System.out.print(" ");
+        System.out.println(quantity); // print variable num2 to console
+        System.out.println(price); // print price to console
+        System.out.println(product_name); // print product name to console
+        System.out.println(tag); // print product tag
+    }
+    
+}
+
+
